6f95c4d32cb9f0ba3eb16e8f4c8953a52914d02b
she11sh0cked <22623152+she11sh0cked@users.noreply.github.com>
2024-11-12 11:09:29 +0100

    refactor(docker): remove redundant environment variable checks in entrypoint script

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,25 +1,5 @@
 #!/usr/bin/env bash
 
-if [ -z "$LASTFM_USERNAMES" ]; then
-    echo "LASTFM_USERNAMES is not set. Exiting."
-    exit 1
-fi
-
-if [ -z "$LASTFM_PLAYLISTS" ]; then
-    echo "LASTFM_PLAYLISTS is not set. Exiting."
-    exit 1
-fi
-
-if [ -z "$SPOTIFY_CLIENT_ID" ]; then
-    echo "SPOTIFY_CLIENT_ID is not set. Exiting."
-    exit 1
-fi
-
-if [ -z "$SPOTIFY_CLIENT_SECRET" ]; then
-    echo "SPOTIFY_CLIENT_SECRET is not set. Exiting."
-    exit 1
-fi
-
 if [ -n "$CRON" ]; then
     echo "$CRON cd /app && bun run start" | crontab -
     crond -f
